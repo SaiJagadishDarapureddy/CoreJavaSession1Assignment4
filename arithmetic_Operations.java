4cbd91d6d0bbe2b61e4e1d7838d8a46064ae2713
SaiJagadishDarapureddy <sai.jagadish.d@gmail.com>
2016-06-07 17:58:07 +0000

    Add files via upload

---
+++ arithmetic_Operations.java
@@ -0,0 +1,11 @@
+
+public class arithmetic_Operations {
+public static void main(String[] args) {
+	int x=10;
+	int y=20;
+	System.out.println(x+y*2);
+	System.out.println(x-y+2);
+	System.out.println((x+y)*2);
+	System.out.println(y%x);
+}
+}
